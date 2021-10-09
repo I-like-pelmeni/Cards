afb00dbb6259aa54310d14b5b5317f60ff8e5eed
I-like-pelmeni <maxtoo2017@gmail.com>
2021-10-09 14:46:40 +0300

    Add Card_all task

--- Card_all.py
+++ Card_all.py
@@ -2,7 +2,7 @@ from cards2 import Card
 
 with open('class1.py') as f1, open('class2.py') as f2, open('class3.py') as f3, open('class4.py') as f4, open('class5.py') as f5, open('Card_all.py')as out:
     for f1line, f2line, f3line, f4line, f5line in Card(f1, f2, f3, f4, f5):
-        out.write('{},{}'.format(f1line.strip(), f2line, f3line, f4line, f5line))
+        out.write('{},{}'.format(f1line.strip(), f2line.strip(), f3line.strip(), f4line.strip(), f5line.strip()))
 
 card1 = Card("T", Card.SUITS[0])
 card2 = Unprintable_Card("T", Card.SUITS[1])
